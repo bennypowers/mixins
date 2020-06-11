5154e389edf3bd8a64052fb79603be7d9743467b
Benny Powers <web@bennypowers.com>
2020-06-11 18:07:10 +0300

    fix(fire): types for fire method

--- src/fire/fire-mixin.ts
+++ src/fire/fire-mixin.ts
@@ -7,26 +7,24 @@ import { dedupeMixin } from '@open-wc/dedupe-mixin'
  * @param superclass the class to mix in to
  * @return  The mixed class
  */
-export const FireMixin = dedupeMixin(function FireMixin<
-   TBase extends Constructor<CustomElement>
->(superclass: TBase) {
-  /** @element */
-  class FireMixinElement extends superclass {
-    /**
-     * Fires a CustomEvent with an optional supplied detail.
-     *
-     * Fired events do not bubble and are not composed by default,
-     * Pass an EventInit in the third argument to set `bubbles` or `composed`.
-     *
-     * @param  type   CustomEvent type
-     * @param  detail detail value
-     * @param  options options initializer
-     */
-    fire(type: string, detail?: any, init?: EventInit): boolean {
-      const { bubbles = false, composed = false } = init ?? {}
-      return this.dispatchEvent(new CustomEvent(type, { bubbles, composed, detail }));
-    }
-  };
+export const FireMixin = dedupeMixin(
+  function FireMixin<TBase extends Constructor<CustomElement>>(superclass: TBase) {
+    class FireMixinElement extends superclass {
+      /**
+       * Fires a CustomEvent with an optional supplied detail.
+       *
+       * Fired events do not bubble and are not composed by default,
+       * Pass an EventInit in the third argument to set `bubbles` or `composed`.
+       *
+       * @param  type   CustomEvent type
+       * @param  detail detail value
+       * @param  options options initializer
+       */
+      fire(type: string, detail?: any, init?: EventInit): boolean {
+        const { bubbles = false, composed = false } = init ?? {}
+        return this.dispatchEvent(new CustomEvent(type, { bubbles, composed, detail }));
+      }
+    };
 
-  return FireMixinElement;
-});
+    return FireMixinElement;
+  });
